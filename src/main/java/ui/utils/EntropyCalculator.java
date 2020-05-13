0f7bcaaad0e94a451fc2d8ded1ccfa626d61bc26
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-13 23:23:04 +0200

    extracted entropy calculation to a separate class

---
+++ src/main/java/ui/utils/EntropyCalculator.java
@@ -0,0 +1,20 @@
+package main.java.ui.utils;
+
+import java.util.Collection;
+
+public class EntropyCalculator {
+
+    public static double execute(Collection<Integer> amounts, int total){
+        double entropy = 0;
+        for (int amount : amounts){
+            double probability = 1.0*amount / total;
+            entropy -= probability * log2(probability);
+        }
+        return entropy;
+    }
+
+    private static double log2(double n) {
+        return Math.log(n)/Math.log(2);
+    }
+
+}
