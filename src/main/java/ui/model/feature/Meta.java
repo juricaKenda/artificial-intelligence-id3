642b20e25ce505fe1c93e94ca5bfb47f327227bd
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-14 16:45:51 +0200

    added meta partitioning

--- src/main/java/ui/model/feature/Meta.java
+++ src/main/java/ui/model/feature/Meta.java
@@ -2,42 +2,74 @@ package main.java.ui.model.feature;
 
 import main.java.ui.utils.EntropyCalculator;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Meta {
-    private HashMap<String,Integer> with;
-    private int partitionSize;
+    private HashMap<String,List<Integer>> with;
+    private List<Integer> partitionCache;
 
     public Meta(){
         with = new HashMap<>();
+        partitionCache = new ArrayList<>();
     }
 
-    public void upsert(String label) {
+    public void upsert(String label,int index) {
         if (!with.containsKey(label)){
-            with.put(label,1);
-        }else {
-            with.put(label,with.get(label)+1);
+            with.put(label,new ArrayList<>());
         }
-        partitionSize++;
+        with.get(label).add(index);
+        partitionCache.add(index);
     }
 
-    @Override
-    public String toString(){
-        return with.toString();
-    }
 
     public Integer partitionSize() {
-        return partitionSize;
+        return partitionCache.size();
     }
 
     public double getEntropy(){
-        return EntropyCalculator.execute(with.values(), partitionSize);
+        return EntropyCalculator.execute(flatToSize(with.values()), partitionSize());
+    }
+
+    private Collection<Integer> flatToSize(Collection<List<Integer>> values) {
+        Collection<Integer> sizes = new ArrayList<>();
+        for (List<Integer> value : values) {
+            sizes.add(value.size());
+        }
+        return sizes;
     }
 
     public int countOrDefault(String value, int defaultVal) {
         if (!with.containsKey(value)){
             return defaultVal;
         }
-        return with.get(value);
+        return with.get(value).size();
+    }
+
+    public List<Integer> partitionIndexes() {
+        return partitionCache;
+    }
+
+    public boolean partitionContainsAny(List<Integer> partition) {
+        for (Integer index : partition) {
+            if (partitionCache.contains(index)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Optional<String> keyForEntry(int index){
+        for (String key : with.keySet()) {
+            List<Integer> indexes = with.get(key);
+            if (indexes.contains(index)){
+                return Optional.ofNullable(key);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public String toString(){
+        return with.toString();
     }
 }
