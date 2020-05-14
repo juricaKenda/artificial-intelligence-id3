0d0015e49194fad478f71b1576df84dc4b557485
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-14 16:45:34 +0200

    added partitioning

--- src/main/java/ui/model/feature/Feature.java
+++ src/main/java/ui/model/feature/Feature.java
@@ -8,37 +8,39 @@ import static java.lang.String.format;
 
 public class Feature {
     private String key;
-    private List<String> entries;
+    private HashMap<Integer,String> entriesByIndex;
     private HashMap<String,Meta> valueMeta;
-    private boolean label = false;
 
     public Feature(String featureKey){
         key = featureKey;
-        entries = new ArrayList<>();
+        entriesByIndex = new HashMap<>();
         valueMeta = new HashMap<>();
     }
 
-    public void markLabel(){
-        label = true;
+    private Feature(String featureKey, HashMap<Integer, String> entriesByIndex, HashMap<String, Meta> valueMeta){
+        this.key = featureKey;
+        this.entriesByIndex = entriesByIndex;
+        this.valueMeta = valueMeta;
     }
 
-    public void bindValueForLabel(String value, String label) {
-        entries.add(value);
-        upsertMeta(value,label);
+
+    public void bindValueForLabel(String value, String label,int index) {
+        entriesByIndex.put(index,value);
+        upsertMeta(value,label,index);
     }
 
-    private void upsertMeta(String value, String label) {
+    private void upsertMeta(String value, String label,int index) {
         if (!valueMeta.containsKey(value)){
             valueMeta.put(value,new Meta());
         }
-        valueMeta.get(value).upsert(label);
+        valueMeta.get(value).upsert(label,index);
     }
 
 
     public double getInformationGain(Set<String> values){
-        double entropy = EntropyCalculator.execute(getValuePartitions(values),entries.size());
+        double entropy = EntropyCalculator.execute(getValuePartitions(values), entriesByIndex.size());
         for (Meta meta : valueMeta.values()){
-            entropy -= meta.getEntropy()*meta.partitionSize()/entries.size();
+            entropy -= meta.getEntropy()*meta.partitionSize()/ entriesByIndex.size();
         }
         return entropy;
     }
@@ -57,14 +59,55 @@ public class Feature {
 
     @Override
     public String toString() {
-        return format("%s [%s] : %s",key,label, valueMeta);
+        return format("%s (%s) : %s",key, entriesByIndex,valueMeta);
     }
 
     public Set<String> getValues() {
         return valueMeta.keySet();
     }
 
-    public String getEntry(int index) {
-        return entries.get(index);
+    public List<List<Integer>> valueIndexes() {
+        List<List<Integer>> indexes = new ArrayList<>();
+        for (Meta meta : valueMeta.values()){
+            indexes.add(meta.partitionIndexes());
+        }
+        return indexes;
     }
+
+    public Feature subsetFrom(List<Integer> partition) {
+        HashMap<Integer,String> partitionedEntries = partitionEntries(partition);
+        HashMap<String, Meta> partitionedMeta = partitionMeta(partition);
+        return new Feature(key,partitionedEntries,partitionedMeta);
+    }
+
+    private HashMap<String, Meta> partitionMeta(List<Integer> partition) {
+        HashMap<String, Meta> partitioned = new HashMap<>();
+        for (String key : valueMeta.keySet()) {
+            Meta meta = valueMeta.get(key);
+            if (meta.partitionContainsAny(partition)){
+                if (!partitioned.containsKey(key)){
+                    partitioned.put(key,new Meta());
+                }
+                for (Integer index : partition) {
+                    meta.keyForEntry(index).ifPresent(entry-> upsertMeta(partitioned, key, index, entry));
+                }
+
+            }
+        }
+        return partitioned;
+    }
+
+    private void upsertMeta(HashMap<String, Meta> partitioned, String key, Integer index, String entry) {
+        partitioned.get(key).upsert(entry, index);
+    }
+
+    private HashMap<Integer, String> partitionEntries(List<Integer> partition) {
+        HashMap<Integer,String> partitioned = new HashMap<>();
+        for (Integer index : partition) {
+            partitioned.put(index, entriesByIndex.get(index));
+        }
+        return partitioned;
+    }
+
+
 }
