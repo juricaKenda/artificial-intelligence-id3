831554b581feb807c6eb6f2a93a5af9416ae09d9
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-14 22:38:32 +0200

    implemented the parser for searchable tuples

--- src/main/java/ui/utils/parser/Parser.java
+++ src/main/java/ui/utils/parser/Parser.java
@@ -1,7 +1,11 @@
 package main.java.ui.utils.parser;
 
+import main.java.ui.id3.model.SearchableTuple;
 import main.java.ui.model.FeatureSet;
 import main.java.ui.utils.iterator.Iterator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Parser {
 
@@ -16,6 +20,37 @@ public class Parser {
 
         iterator.onTermination();
         return featureSet;
+    }
+
+    public List<SearchableTuple> parseTestSet(Iterator iterator){
+        if (!iterator.hasNext()){
+            throw new UnsupportedOperationException("Can not parse an uniterable!");
+        }
+        SearchableTuple keys = parseKeys(iterator.next());
+        List<SearchableTuple> tuples = new ArrayList<>();
+        while (iterator.hasNext()){
+            SearchableTuple tuple = new SearchableTuple();
+            String line = iterator.next();
+            String[] values = parseFeatureValues(line);
+            for (int i=0; i<values.length; i++){
+                String key = keys.get(Integer.toString(i));
+                tuple.bind(key,values[i]);
+            }
+            tuples.add(tuple);
+        }
+
+        iterator.onTermination();
+        return tuples;
+    }
+
+    private SearchableTuple parseKeys(String line) {
+        SearchableTuple tuple = new SearchableTuple();
+        String[] values = parseFeatureValues(line);
+        for (int i=0; i<values.length; i++){
+            String key = Integer.toString(i);
+            tuple.bind(key,values[i]);
+        }
+        return tuple;
     }
 
     private String[] parseFeatureValues(String values) {
