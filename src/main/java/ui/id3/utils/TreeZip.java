dbb233005f93b7cce9895dd836e4d05b8e96147e
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-14 22:01:44 +0200

    added the treezip util

---
+++ src/main/java/ui/id3/utils/TreeZip.java
@@ -0,0 +1,37 @@
+package main.java.ui.id3.utils;
+
+import main.java.ui.id3.model.Model;
+import main.java.ui.model.tree.Tree;
+import main.java.ui.model.tree.TreeElement;
+
+public class TreeZip {
+
+    public static Model zipNode(Tree tree) {
+        Model model = new Model();
+        recursiveZip(tree.root(),0,model);
+        return model;
+    }
+
+    private static void recursiveZip(TreeElement root, int depth, Model model) {
+        zipNode(root,depth,model);
+        for (TreeElement child : root.children()) {
+            recursiveZip(child,depth+1, model);
+        }
+    }
+
+    private static void zipNode(TreeElement root, int depth, Model model) {
+        if (root.labelValue().isPresent()){
+            model.bindResult(root.proxy(),root.labelValue().get());
+            return;
+        }
+        if (root.splitterFeature().isPresent()){
+            String splitter = root.splitterFeature().get().toString();
+            model.bindRequest(root.proxy(), splitter);
+            model.log(depth,splitter);
+            return;
+        }
+        throw new UnsupportedOperationException("Unexpected tree element!");
+    }
+
+
+}
