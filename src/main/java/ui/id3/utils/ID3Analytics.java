4c9d8eed027ad98de0a5e86ee1e63c3e585bd5ac
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-16 21:55:15 +0200

    reusing confusion matrix instead of separate logic

--- src/main/java/ui/id3/utils/ID3Analytics.java
+++ src/main/java/ui/id3/utils/ID3Analytics.java
@@ -1,42 +1,34 @@
 package main.java.ui.id3.utils;
 
 import main.java.ui.Analytics;
+import main.java.ui.model.ConfusionMatrix;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class ID3Analytics implements Analytics {
-    private int labelSize;
-    private int totalChecks;
-    private double correct;
-    private List<String> pairs;
     private List<String> predictions;
     private HashMap<Integer,List<String>> depthLog;
+    private ConfusionMatrix confusionMatrix;
 
-    public ID3Analytics(int labelSize, HashMap<Integer, List<String>> depthLog) {
-        this.labelSize = labelSize;
-        pairs = new ArrayList<>();
+    public ID3Analytics(HashMap<Integer, List<String>> depthLog, List<String> labels) {
         predictions = new ArrayList<>();
         this.depthLog = depthLog;
+        confusionMatrix = new ConfusionMatrix(labels);
     }
 
     public void commit(String prediction, String actual) {
-        if (prediction.equals(actual)){
-            correct++;
-        }
-        pairs.add(actual+prediction);
+        confusionMatrix.commit(prediction,actual);
         predictions.add(prediction);
-        totalChecks++;
     }
 
     @Override
     public void show(){
         showDepthLog();
         showPredictions();
-        showAccuracy();
-        showConfusionMatrix();
+        confusionMatrix.displayAccuracy();
+        confusionMatrix.displayConfusion();
     }
 
     private void showPredictions() {
@@ -47,10 +39,7 @@ public class ID3Analytics implements Analytics {
     private void showDepthLog() {
         StringBuilder builder = new StringBuilder();
         int depth = 0;
-        while (true){
-            if (!depthLog.containsKey(depth)){
-                break;
-            }
+        while (depthLog.containsKey(depth)) {
             List<String> splitters = depthLog.get(depth);
 
             for (String splitter : splitters) {
@@ -66,42 +55,6 @@ public class ID3Analytics implements Analytics {
         System.out.println(result);
     }
 
-    private void showConfusionMatrix() {
-        List<Integer> counts = buildConfusionList();
-        StringBuilder builder = new StringBuilder();
-        int i = 0;
-        for (Integer count : counts) {
-            if (i%labelSize == 0 && i != 0){
-                builder.append("\n");
-            }
-            builder.append(count).append(" ");
-            i++;
-        }
-        System.out.println(builder.toString());
-    }
-
-    private List<Integer> buildConfusionList() {
-        Collections.sort(pairs);
-        String prev = null;
-        int count = 0;
-        List<Integer> counts = new ArrayList<>();
-        for (String pair : pairs) {
-            if (!pair.equals(prev)){
-                if (prev != null){
-                    counts.add(count);
-                }
-                count = 0;
-                prev = pair;
-            }
-            count++;
-        }
-        counts.add(count);
-        return counts;
-    }
-
-    private void showAccuracy() {
-        System.out.println(String.format("\n%2.5f",correct / totalChecks));
-    }
 
 
 }
