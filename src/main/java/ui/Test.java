66b13e21704aae76e48fbde7d1fe259a06418f30
Jurica Kenda <jurica.kenda@gmail.com>
2020-05-16 22:09:11 +0200

    lab samples

--- src/main/java/ui/Test.java
+++ src/main/java/ui/Test.java
@@ -1,9 +1,7 @@
 package main.java.ui;
 
-import main.java.ui.id3.ID3;
 import main.java.ui.id3.model.Config;
 import main.java.ui.id3.model.SearchableTuple;
-import main.java.ui.id3.utils.Analytics;
 import main.java.ui.model.FeatureSet;
 import main.java.ui.utils.iterator.FileIterator;
 import main.java.ui.utils.parser.Parser;
@@ -14,20 +12,14 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        Parser parser = new Parser();
+        Parser parser = Bootstrap.getParser();
         FeatureSet trainSet = parser.parseFeatureSet(new FileIterator("/Users/juricakenda/desktop/lab3_files/datasets/volleyball.csv"));
-        ID3 id3 = new ID3(new Config(1));
-        id3.fit(trainSet);
+        Config config = parser.parseConfig(new FileIterator("/Users/juricakenda/desktop/lab3_files/config/id3.cfg"));
         List<SearchableTuple> tuples = parser.parseTestSet(new FileIterator("/Users/juricakenda/desktop/lab3_files/datasets/volleyball_test.csv"));
-        Analytics analytics = id3.predict(tuples);
-        analytics.show();
-    }
 
-    private static SearchableTuple buildTuple() {
-        SearchableTuple tuple = new SearchableTuple();
-        tuple.bind("weather","sunny");
-        tuple.bind("wind","weak");
-        tuple.bind("humidity","high");
-        return tuple;
+        Runner runner = Bootstrap.getRunner(config);
+        runner.fit(trainSet);
+        Analytics analytics = runner.predict(tuples);
+        analytics.show();
     }
 }
